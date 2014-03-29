0abf7cc501327b87ebeb930df192f745bae02155
Komsan Kamsamur <maprangzth@hotmail.com>
2014-03-29 13:51:36 +0700

    Edit View to column name re place id

--- app/protected/views/deviceBrand/_view.php
+++ app/protected/views/deviceBrand/_view.php
@@ -5,17 +5,12 @@
 
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('device_brand_name')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->device_brand_name), array('view', 'id'=>$data->id)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('device_type_id')); ?>:</b>
 	<?php echo CHtml::encode($data->device_types->device_type_name); ?>
 	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('device_brand_name')); ?>:</b>
-	<?php echo CHtml::encode($data->device_brand_name); ?>
-	<br />
-
 
 </div>
--- app/protected/views/deviceBrand/view.php
+++ app/protected/views/deviceBrand/view.php
@@ -16,7 +16,7 @@ $this->menu=array(
 );
 ?>
 
-<h1>View DeviceBrand #<?php echo $model->id; ?></h1>
+<h1>View DeviceBrand #<?php echo $model->device_brand_name; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
